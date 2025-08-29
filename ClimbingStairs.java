8cfcf2b5723822c85761f4f1c1340250dff59d0e
Mohammed Malik <malikmohammed2067@gmail.com>
2025-08-29 02:23:31 -0700

    Create ClimbingStairs.java

---
+++ ClimbingStairs.java
@@ -0,0 +1,17 @@
+class ClimbingStairsProblem{
+    public static int ClimbingStairs(int n){
+       if(n==1) return 1;
+       if(n==2) return 2;
+      int first=1,second=2,ways=0;
+      for(int i=3;i<=n;i++){
+          ways=first+second;
+          first=second;
+          second=ways;
+      }
+      return second;
+    }
+    public static void main(String[] args){
+        int n=3;
+        System.out.print(ClimbingStairs(n));
+    }
+}
